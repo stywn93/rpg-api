f65a620d080f9ea6edd00f2e3a2c71c05569a93e
Iwan Bagus S <stywn93@gmail.com>
2026-02-16 17:52:31 +0700

    success create auth endpoint

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('api/v1', ['namespace' => 'App\Controllers\Api\V1'], function($routes){
+
+    $routes->post('auth/register', 'AuthController::register');
+    $routes->post('auth/login', 'AuthController::login');
+
+});
