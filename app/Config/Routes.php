de3b54c20cd6e0ebb20e3181bf564817dd07f7c9
iwan bagus s <iwan@situbondokab.go.id>
2026-02-24 09:44:46 +0700

    404 override

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,3 +16,5 @@ $routes->group('api/v1', ['namespace' => 'App\Controllers\Api\V1', 'filter' => '
     $routes->post('users/(:num)/activate/', 'UserController::activate/$1');
     $routes->post('users/(:num)/suspend', 'UserController::suspend/$1');
 });
+
+$routes->set404Override('\App\Controllers\Api\V1\ErrorController::notFound');
