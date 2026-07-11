de4222f7215655cb35c360f4a8c7ede489649083
Iwan Bagus S <stywn93@gmail.com>
2026-07-12 03:54:29 +0700

    fix: remove unused methods and clean up dead code in models

--- app/Models/QueueLogModel.php
+++ app/Models/QueueLogModel.php
@@ -37,12 +37,4 @@ class QueueLogModel extends Model
     | Join dengan user & queue
     |--------------------------------------------------------------------------
     */
-    public function getLogsWithUser($queueId)
-    {
-        return $this->select('queue_logs.*, users.name as changed_by_name')
-            ->join('users', 'users.id = queue_logs.changed_by')
-            ->where('queue_id', $queueId)
-            ->orderBy('changed_at', 'ASC')
-            ->findAll();
-    }
 }
